bf923403639b78dae31ea696107dd9905b8ad757
Gant <GantMan@gmail.com>
2015-06-03 09:25:37 -0500

    added more event wrappers and began event handling tree

--- lib/project/ruby_motion_query/rmq/events.rb
+++ lib/project/ruby_motion_query/rmq/events.rb
@@ -1,28 +1,42 @@
 class RMQ
   def on(event, args={}, &block)
     self.selected.each do |view|
-
       case event
       when :click, :tap, :touch
-        view.onClickListener = RMQClickHandler.new(&block)
+        handle_click view
+      when :change
+        handle_change view
       else
         raise "[RMQ ERROR] Unrecognized event: #{event}"
       end
-
     end
 
   end
-end
 
-class RMQClickHandler
-  attr_accessor :click_block
+  private
 
-  def initialize(&block)
-    @click_block = block
+  def handle_click view
+    # Click event for ListItems
+    if view.respond_to? :setOnItemClickListener
+      view.onItemClickListener = RMQItemClick.new(&block)
+    # Generic Click
+    else
+      view.onClickListener = RMQClick.new(&block)
+    end
   end
 
-  def onClick(view)
-    click_block.call(view)
+
+  def handle_change view
+    # Seek bar change
+    if view.respond_to? :setOnSeekBarChangeListener
+      view.onSeekBarChangeListener = RMQSeekChange.new(args, &block)
+    # Text change
+    elsif view.respond_to? :addTextChangeListener
+      view.addTextChangedListener(RMQTextChange.new(&block))
+    end
   end
 
 end
+
+
+
