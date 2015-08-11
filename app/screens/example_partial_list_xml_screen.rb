3af1fec71388e883ec82ab022426a67ed6018180
Gant <GantMan@gmail.com>
2015-08-11 14:53:53 -0500

    added example to home screen

--- app/screens/example_partial_list_xml_screen.rb
+++ app/screens/example_partial_list_xml_screen.rb
@@ -2,21 +2,21 @@ class ExamplePartialListXML < PMListScreen
   xml_layout :embedded_listview
   title "View with a ListView Inside"
 
-  def on_load
-    mp "ExamplePartialListXML on_load"
-  end
-
-  def add_adapter
-    find!(:some_internal_listview).adapter = adapter
-  end
-
   def table_data
     [{
-      title: "Regular Listview inside XML",
-      cells: [{
-        title: "I'm a Regular Cell"
-      }]
+      cells: buncho_cells
     }]
+  end
+
+
+  private
+
+  def buncho_cells
+    array_of_cells = []
+    100.times do |i|
+      array_of_cells << {title: "Here's cell #{i}"}
+    end
+    array_of_cells
   end
 
 end
